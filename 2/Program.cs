d403c6e9c523cede279c8ada6a8bb6843499e71a
ArtemOg <ar.ogurec88@gmail.ru>
2023-12-11 19:24:15 +0300

    ДЗ по фунциям

---
+++ 2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 2: Задайте массив заполненный случайными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+Random rnd = new Random();
+Console.WriteLine("Введите длину массива: ");
+int len = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[len];
+int k=0;
+for (int i=0 ; i<len; i++){
+    array[i]= rnd.Next(100,1000);
+    Console.Write($"{array[i]} ");
+    if (array[i]%2==0) k+=1;
+}
+Console.WriteLine();
+Console.WriteLine(k);
---
+++ 3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 3: Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+Random rnd = new Random();
+Console.WriteLine("Введите длину массива: ");
+int len = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[len];
+int k=0;
+for (int i=0 ; i<len; i++){
+    array[i]= rnd.Next(1,10);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+int[] arr = new int[len];
+for ( int i =0; i<len;i++){
+    int n = array[i];
+    arr[i]=array[len-i-1];
+    array[len-i-1]=n;
+} 
+for (int i = 0; i<len;i++){
+    Console.Write($"{arr[i]} ");
+}
